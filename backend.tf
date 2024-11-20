52ca210b4575357a0e3e73e9b6880fc247df3c8c
gad0788 <156004978+gad0788@users.noreply.github.com>
2024-11-20 16:00:01 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "wk10-gn-terraform"
+    bucket         = ""
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "state-log"
+    dynamodb_table = ""
   }
 }
